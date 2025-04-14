f4dff37cb4009ed4c198dbd01fd0fe72ba9dd836
Gouri S Nair <gourisnair24805@gmail.com>
2025-04-15 03:55:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function buttonclick(val){
+
+ document.getElementById("screen").value+=val
+}
+function clearscreen(val){
+    document.getElementById("screen").value=""
+}
+
+function eqclicck(){
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
